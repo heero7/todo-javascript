9697fc67aa3e9389335cd5d0cacc32d37dc6ee28
heero7 <kevin7.mudia@gmail.com>
2024-01-26 17:48:50 -0600

    add todo routes in separate file

---
+++ src/todoRoutes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {FastifyInstance} fastify instance.
+ * @param {Object} options for the plugin.
+ */
+export async function todoRoutes(fastify) {
+    const collection = fastify.mongo.db.collection("Todos");
+
+    fastify.get("/", async () => {
+        return "Todo Application";
+    });
+
+    fastify.get("/todos", async () => {
+        const result = await collection.find().toArray();
+        return result;
+    });
+
+    fastify.post("/todos", async () => {
+    });
+}
+
