cbadbdd48f8b2b1f1cb3ff22390f1761954b26b3
heero7 <kevin7.mudia@gmail.com>
2024-01-28 02:34:52 -0600

    todo routes are only accessible with an access token

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 For whatever reason the docs don't really describe how to connect
 to your databsase. The connection string should look like this
 `"mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<db-name>?retryWrites=true&w=majority"`
+<u>Key parts:</u>
+- username: add a user from the Atlas 
+- password: password for the user
+- <u>db-name</u>: *Don't forget this, otherwise you won't connect to the DB*
--- src/todoRoutes.js
+++ src/todoRoutes.js
@@ -1,15 +1,36 @@
+import middie from "@fastify/middie";
+import jwt from "jsonwebtoken";
 /**
  * @param {FastifyInstance} fastify instance.
  * @param {Object} options for the plugin.
  */
 export async function todoRoutes(fastify) {
-    const collection = fastify.mongo.db.collection("Todos");
+    await fastify.register(middie);
+    fastify.addHook("onRequest", async (request, reply) => {
+        if (!request.headers || !request.headers.authorization) {
+            fastify.log.error("Cannot access this route without authorization.");
+            reply.code(401).send();
+            return;
+        }
 
-    fastify.get("/", async () => {
-        return "Todo Application";
+        const authTokenHeader = request.headers.authorization;
+        const authToken = authTokenHeader.replace("Bearer ", "");
+        try {
+            jwt.verify(authToken, process.env.JWT_SECRET);
+            // todo: check if the token has expired before allowing access.
+            fastify.log.info("Auth verified.");
+        } catch (e) {
+            fastify.log.error("Just to let you know, that token didn't work.");
+            fastify.log.error(e.message);
+            reply.code(401).send();
+            return;
+        }
     });
 
+    const collection = fastify.mongo.db.collection("Todos");
+
     fastify.get("/todos", async () => {
+        console.log("Getting the todos");
         const result = await collection.find().toArray();
         return result;
     });
