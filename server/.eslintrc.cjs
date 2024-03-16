b2e99346da9ee64422e5703ed7a0813ff63b1b78
heero7 <kevin7.mudia@gmail.com>
2024-03-15 23:50:57 -0500

    refactors client to get working

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true 
+  },
+  "extends": "eslint:recommended",
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+}
