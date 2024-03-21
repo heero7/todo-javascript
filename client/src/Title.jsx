06118d44174229b828e95a2db43b3097f99304d5
heero7 <kevin7.mudia@gmail.com>
2024-03-20 23:27:47 -0500

    update sidebar

---
+++ client/src/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+    return (
+        <div className="flex flex-row justify-center">
+            <h1>Things Todo</h1>
+        </div>
+    );
+};
+
+export default Title;
---
+++ client/src/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+    return (
+        <div>
+            <h3>Todo Name</h3>
+            <h3>Completed?</h3>
+        </div>
+    );
+};
+
+export default TodoItem;
